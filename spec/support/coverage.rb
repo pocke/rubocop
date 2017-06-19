bc29a1f922aa0f33cbcf22d5992b25ce6e8d2156
Masataka Kuwabara <kuwabara@pocke.me>
2017-06-19 09:20:39 +0900

    Parallelize RSpec by using test-queue on Travis CI

--- spec/support/coverage.rb
+++ spec/support/coverage.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if ENV['TRAVIS'] || ENV['COVERAGE']
+if (ENV['TRAVIS_BRANCH'] == 'master' && ENV['TRAVIS_PULL_REQUEST'] == 'false') || ENV['COVERAGE']
   require 'simplecov'
   SimpleCov.add_filter '/spec/'
   SimpleCov.add_filter '/vendor/bundle/'
